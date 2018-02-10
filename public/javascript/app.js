f3e84b85f683ede843510c195b8fe3faf6d77558
Francisco Mercado <sissyyayle0517@hotmail.com>
2018-02-09 19:05:13 -0600

    fixing alerts

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,48 @@
+// Incorporacion de vue.js
+
+var vm = new Vue({
+  el: '#form-contact',
+  data:{
+    sercyn:{
+      price:' El costo de tu servicio es de $',
+      name:'',
+      email:'',
+      phone:'',
+      persons: 0,
+      place: null
+    }
+  },
+  computed: {
+    costoDelViaje: function () {
+      var place= this.sercyn.place;
+      var price= this.sercyn.price;
+      var person= this.sercyn.persons;
+
+      if(this.sercyn.persons <= 7 && place == 'rio') {
+        return price + 1800
+      }
+      else if(person > 7 && place == 'rio') {
+        return price + 3600
+      }
+      else if(person <=7 && place == 'arrecife') {
+        return price + 3500
+      }
+      else if(person > 7 && place == 'arrecife') {
+        return price + 7000
+      }
+      else if(person <= 7 && place == 'isla') {
+        return price + 5500
+      }
+      else if(person > 7 && place == 'isla') {
+        return price + 11000
+      }
+    } 
+  }
+})
+
 
 // Alerta de envio de formulario
-$('#form-contact').submit(function() {
+$('.form-contact').submit(function() {
     swal('Muchas Gracias!', 'Tu mensaje ha sido enviado', 'success')
   .fail(function(){
     swal('Lo sentimos!','Hubo un error en el envío', 'error')
