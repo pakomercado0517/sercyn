25cd0446ec68a82b4d6aad15796a0ebdebeeaf6d
Francisco Mercado <sissyyayle0517@hotmail.com>
2018-04-18 08:55:09 -0500

    update the prices

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -20,7 +20,7 @@ var vm = new Vue({
       var item1= 2000;
       var item2= 4000;
       var item3= 4200
-      var item4= 8000
+      var item4= 8400
       var item5= 6000
       var item6= 12000
 
