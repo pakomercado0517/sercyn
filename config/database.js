6eb1d9ae7489ddcc5922c551aba3d49cac901ae9
Francisco Mercado <sissyyayle0517@hotmail.com>
2018-07-19 00:17:45 -0500

    fixing troubles login

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports= {
-  // 'url': 'mongodb://localhost/conanpLogin'
-  'url': 'mongodb://sissyyayle0517:chisqueado0517@ds257589.mlab.com:57589/conanp-services'
+  'url': 'mongodb://localhost/conanpLogin'
+  // 'url': 'mongodb://sissyyayle0517:chisqueado0517@ds257589.mlab.com:57589/conanp-services'
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const sendMailer= require('../models/email')
+let Service= require('../models/service')
+const mongoose= require('mongoose')
+let User= require('../models/user')
+let passport= require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,6 +31,74 @@ router.get('/shopping', function (req,res, next) {
 })
 router.get('/privacy', function(req, res) {
 	res.render('privacy')
+})
+
+// login and signup
+
+router.get('/acceso', (req, res) => {
+  res.render('acceso')
+})
+router.get('/login', (req,res) => {
+  res.render('login', {
+    message: req.flash('loginMesage')
+  })
+})
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/service',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+router.get('/signup', (req,res) => {
+  res.render('signup', {
+    message: req.flash('signupMessage')
+  })
+})
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/service',
+  failureRedirect: '/signup',
+  failureFlash: true
+}))
+
+router.get('/logout', (req,res) => {
+  req.logout()
+  res.redirect('/acceso')
+})
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next()
+  }
+  return res.redirect('/acceso')
+}
+
+// crud services
+
+
+router.get('/service', isLoggedIn, (req, res, next) => {
+  Service.find((err, services) => {
+    if (err) throw err
+    res.render('service', {services:services})
+  })
+  })
+router.get('/service/nuevo', isLoggedIn, (req,res,next) => {
+  res.render('serviceForm')
+})
+
+router.get('/service/modificar/:id', isLoggedIn, (req, res, next) => {
+  let idService= req.params.id
+  Service.findOne({_id: idService}, (err, service) => {
+    if (err) throw err
+    res.render('serviceForm', {service: service})
+  })
+})
+
+router.get('/service/eliminar/:id', isLoggedIn, (req, res, next) => {
+  let idService= req.params.id
+  Service.remove({_id: idService}, (err) => {
+    if(err) throw err
+    res.redirect('/service')
+  })
 })
 
 module.exports = router;
