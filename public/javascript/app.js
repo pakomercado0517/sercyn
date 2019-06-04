70007081ff3825272d538245298ba1ea8558c507
Francisco Mercado <sissyyayle0517@hotmail.com>
2019-06-04 10:08:50 -0500

    compressed images from background

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -18,12 +18,12 @@ var vm = new Vue({
       var place= this.sercyn.place;
       var price= this.sercyn.price;
       var person= this.sercyn.persons;
-      var item1= 2000;
-      var item2= 4000;
-      var item3= 4000
-      var item4= 8000
-      var item5= 6000
-      var item6= 12000
+      var item1= 3000;
+      var item2= 6000;
+      var item3= 4200
+      var item4= 8400
+      var item5= 7000
+      var item6= 14000
 
       if(this.sercyn.persons == 0 && place == place) {
         return null
